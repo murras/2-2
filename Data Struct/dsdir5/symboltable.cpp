ed56b8e074641a559d82bf0bf1f0b345b3526a54
Murra <gud2169@naver.com>
2017-10-14 19:25:54 +0900

    Revert "Revert "17.10.14 ds5 제""
    This reverts commit ec342b05330de91bf15caccf2a3169d42862d429.

--- Data Struct/dsdir5/symboltable.cpp
+++ Data Struct/dsdir5/symboltable.cpp
@@ -3,42 +3,41 @@
 #include <map>
 #include <cstring>
 using namespace std;
-struct cmp_str{
-    bool operator() (char const *a, char const *b){
-        return strcmp(a,b) < 0;
-    }
+
+struct cmp_str {
+	bool operator()(char const *a,char const *b) const {
+		return strcmp(a, b) < 0;
+	}
 };
 
 map<char*, float, cmp_str> ST; // Symbol Table
 
-void PrintValue(char *name){
-    map<char*, float, cmp_str>::iterator iter;
-    iter = ST.find(name);
-    if(iter == ST.end())
-        cout << name << " does not exist" << endl;
-    else
-        cout << iter->first << " " << iter->second << endl;
+
+void PrintValue(char *name) {
+	map<char*, float, cmp_str>::iterator iter;
+	iter = ST.find(name);
+	if (iter == ST.end())
+		cout << name << " does not exist" << endl;
+	else
+		cout << iter->first << " " << iter->second << endl;
 }
 
-void StoreIntoST(char *name, float val){
-    // Symbol Table에 name을 키로 val을 값으로 하는 pair를 저장하라
-    map<char *, float, cmp_str>::iterator iter;
-    iter = ST.find(name);
-    if(iter != ST.end())
-        ST.erase(name); // 기존의 저장을 제거
-    ST[name] = val;
-    // ST.insert(pair<char *, float>(name, val)) 다른 저장 방법
+void StoreIntoST(char *name, float val) {
+	map<char*, float, cmp_str>::iterator iter;
+	iter = ST.find(name);
+	if (iter != ST.end())
+		ST.erase(name); // 기존의 저장을 제거
+	ST[name] = val;
+	// ST.insert(pair<char *, float>(name, val)) 다른 저장 방법
 }
 
-int main(){
-    StoreIntoST((char *)"abro", 23);
-    StoreIntoST((char *)"bb", 34.5);
-    StoreIntoST((char *)"haha", 99);
-    StoreIntoST((char *)"bb", 1234.5);
+int main() {
+	StoreIntoST((char *)"abro", 23);
+	StoreIntoST((char *)"bb", 34.5);
+	StoreIntoST((char *)"haha", 99);
+	StoreIntoST((char *)"bb", 1234.5);
 
-    PrintValue((char *)"bb");   
-    PrintValue((char *)"haha");
-    PrintValue((char *)"hoho");
+	PrintValue((char *)"bb");
+	PrintValue((char *)"haha");
+	PrintValue((char *)"hoho");
 }
-
-
